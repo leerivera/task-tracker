9f15c0cdbe4406e2a64b89402f0ccde541e6ebdf
leerivera <leerivera80@gmail.com>
2022-11-06 17:40:14 -0500

    innerhtml

--- ticket_tracker-main/main.js
+++ ticket_tracker-main/main.js
@@ -1,65 +1,64 @@
 
-document.getElementById("issueInputForm").addEventListener(('submit', saveIssue))
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue)
 
-
-function fetchIssues () {
-    let issues = JSON.parse(localStorage.get('issue'))
+function fetchIssues() {
+    let issues = JSON.parse(localStorage.getItem('issues'))
     let issuesList = document.getElementById('issuesList')
     console.log(issues)
 
     issuesList.innerHTML = '';
 
-    for (let i =0; i < issues.length; i++) {
+    for (let i = 0; i < issues.length; i++) {
         let id = issues[i].id
         let subject = issues[i].subject
         let description = issues[i].description
         let severity = issues[i].severity
         let assignedTo = issues[i].assignedTo
         let status = issues[i].status
-        let statusColor = status == 'Closed' ? 'label-success' : 'label-info'
+        let statusColor = status == "Closed" ? 'label-success' : 'label-info'
 
         issuesList.innerHTML +=
             '<div class="well">' +
-            '<h6> Issue ID:' + id + '</h6' +
-             '<p><span class="label' + statusColor + ' ">' + status + '</span></p>' +
+            '<h6>Issue ID:' + id + '</h6>' +
+            '<p><span class= "label ' + statusColor + ' ">' + status + '</span></p>' +
             '<h3>' + subject + '</h3>' +
             '<p>' + description + '</p>' +
-            '</div>'
-
+            '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '' +
+            '<span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
+            '<a href="#" class="btn btn-warning" onclick=" setStatusClosed(\' '+id+'\')">Close</a>' +
+                '<a href="#" class = "btn" '
+            + '</div>'
     }
-
 }
 
 function saveIssue(e) {
-    let issueId = chance.guid()  //generates id for us
-    let issueSubject = document.getElementById("issueSubjInput").value
-    let issueDesc = document.getElementById("issueDescInput").value
-    let issueSeverity = document.getElementById("issueSeverityInput").value
-    let issueAssignedTo = document.getElementById("issueAssignedToInput").value
+    let issueId = chance.guid()
+    let issueSubject = document.getElementById('issueSubjInput').value
+    let issueDesc = document.getElementById('issueDescInput').value
+    let issueSeverity = document.getElementById('issueSeverityInput').value
+    let issueAssignedTo = document.getElementById('issueAssignedToInput').value
     let issueStatus = 'Open'
 
     let issue = {
         id: issueId,
         subject: issueSubject,
         description: issueDesc,
-        severity: issueSubject,
+        severity: issueSeverity,
         assignedTo: issueAssignedTo,
         status: issueStatus
     }
 
-    if(localStorage.getItem('issues') ===null){
+    if(localStorage.getItem('issues')===null) {
         let issues = []
-
         issues.push(issue)
-
         localStorage.setItem('issues', JSON.stringify(issues))
     } else {
-        let issues = JSON.parse(localStorage.getItem('issues')
-        issue.push(issue)
+        let issues = JSON.parse(localStorage.getItem('issues'))
+        issues.push(issue)
         localStorage.setItem('issues', JSON.stringify(issues))
     }
 
-    document.getElementById('issueInputForm').reset()
+    document.getElementById('issueInputForm').reset();
 
     fetchIssues()
 
