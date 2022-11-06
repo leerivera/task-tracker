58594d0a573def32b09e7ef01b23c7a9f4af166f
leerivera <leerivera80@gmail.com>
2022-11-06 17:01:40 -0500

    finished save function

---
+++ ticket_tracker-main/main.js
@@ -0,0 +1,60 @@
+
+document.getElementById("issueInputForm").addEventListener(('submit', saveIssue))
+
+
+function fetchIssues () {
+    let issues = JSON.parse(localStorage.get('issue'))
+    let issuesList = document.getElementById('issuesList')
+
+    issuesList.innerHTML = '';
+
+    for (let i =0; i < issues.length; i++) {
+        let id = issues[i].id
+        let subject = issues[i].subject
+        let description = issues[i].description
+        let severity = issues[i].severity
+        let assignedTo = issues[i].assignedTo
+        let status = issues[i].status
+        let statusColor = status == 'Closed' ? 'label-success' : 'label-info'
+
+        // issuesList,innerHTML
+
+    }
+
+}
+
+function saveIssue(e) {
+    let issueId = chance.guid()  //generates id for us
+    let issueSubject = document.getElementById("issueSubjInput").value
+    let issueDesc = document.getElementById("issueDescInput").value
+    let issueSeverity = document.getElementById("issueSeverityInput").value
+    let issueAssignedTo = document.getElementById("issueAssignedToInput").value
+    let issueStatus = 'Open'
+
+    let issue = {
+        id: issueId,
+        subject: issueSubject,
+        description: issueDesc,
+        severity: issueSubject,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if(localStorage.getItem('issues') ===null){
+        let issues = []
+
+        issues.push(issue)
+
+        localStorage.setItem('issues', JSON.stringify(issues))
+    } else {
+        let issues = JSON.parse(localStorage.getItem('issues')
+        issue.push(issue)
+        localStorage.setItem('issues', JSON.stringify(issues))
+    }
+
+    document.getElementById('issueInputForm').reset()
+
+    fetchIssues()
+
+    e.preventDefault()
+}
